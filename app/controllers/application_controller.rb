ff0f5cec22974a499ca071402afd12e76f3e9dc0
LeonardoKen Orihara <leonardo.mbc17@gmail.com>
2015-11-19 23:59:37 +0900

    ログインチェックを機能させた

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  #before_filter :login_check
+  before_filter :login_check
   protect_from_forgery with: :exception
 
   private
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
     c = params[:controller]
     a = params[:action]
     if (c != "log" && a != "in") && (c != "users")
-      if session[:userID].blank?
+      if session[:user].blank?
         redirect_to :controller => "log", :action => "in"
       end
     end
